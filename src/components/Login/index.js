b0316055683f8b24d4a7dc102508474bff4f4452
Pedro Giampietro <pedrogiampietro@hotmail.com>
2022-01-06 18:03:10 -0300

    add: component login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,9 +1,29 @@
 import { Container } from '../Layout';
+import styles from './Login.module.css';
 
 export function Login() {
   return (
     <Container>
-      <h1>Login</h1>
+      <div className={styles.loginContainer}>
+        <h1 className={styles.loginTitle}>Autenticação</h1>
+
+        <form className={styles.loginForm}>
+          <div className={styles.loginFormGroup}>
+            <input className={styles.loginInput} type="text" required />
+            <span className={styles.highlight}></span>
+            <span className={styles.bar}></span>
+            <label className={styles.loginLabel}>Usuário</label>
+          </div>
+
+          <div className={styles.loginFormGroup}>
+            <input className={styles.loginInput} type="password" required />
+            <span className={styles.highlight}></span>
+            <span className={styles.bar}></span>
+            <label className={styles.loginLabel}>Senha</label>
+          </div>
+          <button className={styles.loginButton}>Entrar</button>
+        </form>
+      </div>
     </Container>
   );
 }
